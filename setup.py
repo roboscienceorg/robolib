39c09dda513ae2ab91c4879cbd0b83f073084e22
Aaron Campbell <aaron.campbell@mines.sdsmt.edu>
2019-01-08 20:21:07 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(  name='RoboLib: An Educational Robotics Library',
         version='0.0.1',
-        description="This library is an educational resource
-        associated with the open-source RoboScience Organization
-        (found at http://www.roboscience.org/) to provide a useful
-        set of classes and methods to facilitate robotics education.",
+        description="This library is an educational resource" +
+        "associated with the open-source RoboScience Organization" +
+        "(found at http://www.roboscience.org/) to provide a useful" +
+        "set of classes and methods to facilitate robotics education.",
         url='http://www.roboscience.org',
         packages=find_packages()
     )
