f663fabe472ce25587d77e62cd9c40ff14766e76
Aaron Campbell <AaronMC2012@gmail.com>
2019-07-20 18:05:34 -0400

    Clean up the sensor class and finish documentation

--- robolib/tools/sensor.py
+++ robolib/tools/sensor.py
@@ -6,29 +6,40 @@ class Sensor_Error(Exception):
 
 class Sensor():
     '''
-    This class represents
+    This class represents a basic sensor. It allows for a user to provide
+    an error profile and then take readings based on the actual measurements.
+    This could be use in conjunction with a robot model to generate sensory
+    data read from a simulated robot.
     '''
 
     def __init__(self, mean, cov ):
         '''
-        This function
+        This function instantiates the sensor. Currently, the sensor takes
+        in simply a mean and covariance. These may be either numbers or 
+        arrays for higher dimensional sensors.
 
         Parameters
         ----------
-        sensor_set:  Tupple
-            2D list containing the set of sensor information
-        n:
-            The number of sensors
-        m:
-            The number of readings from the sensors
+        mean:  Numeric or 1D Array of Numerics
+            This is the mean of the Gaussian distribution which the sensor 
+            errors will be pulled from. An array indicates a multi-variate
+            distribution.
+        cov:
+            The covariance matrix of the Gaussian distribution which the 
+            sensor erros will be pulled from. It must be symmetric and 
+            positive-semidefinite for proper sampling (from Scipy).
 
         Returns
         -------
-        SimSensors object with the given parameters
+        A Sensor object
         '''
 
-        self._mean = np.array(mean)
-        self._cov = np.array(cov)
+        try:
+            self._mean = np.array(mean)
+            self._cov = np.array(cov)
+        except:
+            return Sensor_Error("Either the mean or covariance could not be \
+                    coerced to a NumPy array. Please check your values.")
 
         n = self._mean.shape[0]
 
@@ -38,18 +49,36 @@ class Sensor():
         self._dimension = n
 
 
-
     @property
     def mean(self):
         """
-        The
+        Getter for the error distribution mean.
+
+        Returns
+        -------
+        mean:
+            The mean of the distribution
         """
         return self._mean
 
     @mean.setter
     def mean(self, value):
+        """
+        Setter for the error distribution mean.
+
+        Parameters
+        ----------
+        value:
+            The value the mean(s) should be set to.
+
+        Raises
+        ------
+        Sensor_Error:
+            when the provided mean(s) don't match the sensor dimension.
+        """
         if value.shape[0] != self.dimension:
-            raise Sensor_Error("Provided mean does not match the Sensor Dimension")
+            raise Sensor_Error("Provided mean(s) does not match \
+                    the Sensor's dimension")
 
         else:
             self._mean = value
@@ -57,12 +86,31 @@ class Sensor():
     @property
     def cov(self):
         """
-        The
+        Getter for the error distribution covariance.
+
+        Returns
+        -------
+        cov:
+            The covariance of the distribution.
         """
         return self._cov
 
     @cov.setter
     def cov(self, value):
+         """
+        Setter for the error distribution covariance matrix.
+
+        Parameters
+        ----------
+        value: Numeric, Array of Numeric
+            The value the covariance(s) should be set to.
+
+        Raises
+        ------
+        Sensor_Error:
+            when the provided covariance matrix don't match the sensor dimension.
+        """
+
         if value.shape[0] != self.dimension or value.shape[1] != self.dimension:
             raise Sensor_Error("Provided covariance matrix does not match the Sensor Dimension")
 
@@ -72,19 +120,38 @@ class Sensor():
     @property
     def dimension(self):
         """
-        The
+        Getter for the Sensor dimension.
+
+        Returns
+        -------
+        dimension:
+            The dimension of the Sensor.
         """
         return self._dimension
 
     def sense( self, orig_data ):
         '''
-        This function
+        This function allows the user to use the instantiated Sensor to
+        sense actual data. This simply adds error values to the presented
+        data based on the Sensor's error profile.
 
         Parameters
         ----------
+        orig_data: Array of Numerics
+            This is the data which the sensor should add error to. The dimensions
+            of the data should be compatible with the Sensor's dimensions. Each
+            row of the array is treated as another data point.
 
         Returns
         -------
+        data:
+            This is the new data with the errors added to each reading. This will
+            be the same size as orig_data.
+
+        Raises
+        ------
+        Sensor_Error:
+            when the data is not compatible with the Sensor's dimensions.
         '''
 
         data = np.array(orig_data)
